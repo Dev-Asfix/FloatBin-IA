d58275d3a06b517c85c5d55a14a386f3dafa32b1
Dev Asfix <rosasramirez753@gmail.com>
2025-01-23 16:25:11 -0500

    Stack

--- dashboard/main.js
+++ dashboard/main.js
@@ -297,38 +297,38 @@ const Tools = () => {
       icon: "fa-solid fa-cloud-sun",
       id: 1,
       image: "https://images.unsplash.com/photo-1492011221367-f47e3ccd77a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-      label: "Weather",
-      name: "Cloudly"
+      label: "Monitoreo: Tiempo real",
+      name: "Gestión Smart"
     }, {
       icon: "fa-solid fa-calculator-simple",
       id: 2,
       image: "https://images.unsplash.com/photo-1587145820266-a5951ee6f620?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2FsY3VsYXRvcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      label: "Calc",
-      name: "Mathio"
+      label: "Llenado: Preciso",
+      name: "Predicción Exacta"
     }, {
       icon: "fa-solid fa-piggy-bank",
       id: 3,
       image: "https://images.unsplash.com/photo-1579621970588-a35d0e7ab9b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YmFua3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      label: "Bank",
-      name: "Cashy"
+      label: "Sostenibilidad:",
+      name: "Eco-Eficiencia"
     }, {
       icon: "fa-solid fa-plane",
       id: 4,
       image: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWlycGxhbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-      label: "Travel",
-      name: "Fly-er-io-ly"
+      label: "Alertas: Instantáneas",
+      name: "Notificaciones Rápidas"
     }, {
       icon: "fa-solid fa-gamepad-modern",
       id: 5,
       image: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dmlkZW8lMjBnYW1lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      label: "Games",
-      name: "Gamey"
+      label: "Fecha - Duracion",
+      name: "Registro Estados"
     }, {
       icon: "fa-solid fa-video",
       id: 6,
       image: "https://images.unsplash.com/photo-1578022761797-b8636ac1773c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHZpZGVvJTIwY2hhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-      label: "Video Chat",
-      name: "Chatty"
+      label: "Seguimiento: Inmediato",
+      name: "Control Total"
     }].map((tool) => {
       const styles = {
         backgroundImage: `url(${tool.image})`
@@ -381,7 +381,7 @@ const Restaurants = () => {
             React.createElement("span", { className: "restaurant-card-desc" }, restaurant.desc)))));
     });
   };
-  return (React.createElement(MenuSection, { icon: "fa-regular fa-pot-food", id: "restaurants-section", title: "Get it delivered!" }, getRestaurants()));
+  return (React.createElement(MenuSection, { icon: "fa-regular fa-pot-food", id: "restaurants-section", title: "Get Started" }, getRestaurants()));
 };
 
 const Movies = () => {
